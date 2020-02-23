cbb4dbe346cb8dfe3a5a4f705655aa4f71fe7cc9
mickykebe <mickykebe@gmail.com>
2020-02-23 13:27:25 +0300

    added react router to the mix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Home from "./Home";
+import PendingListings from "./PendingListings";
 
 const theme = {};
 
@@ -10,7 +12,16 @@ function App() {
     <Fragment>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <Home />
+        <Router>
+          <Switch>
+            <Route path="/pending">
+              <PendingListings />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </ThemeProvider>
     </Fragment>
   );
---
+++ client/src/PendingListings.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PendingListings() {
+  return <div>Pending listings</div>;
+}
